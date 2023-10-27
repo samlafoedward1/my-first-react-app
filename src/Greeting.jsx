5b5335e1da03af21f35e34fb47ef17af0ac19240
Edward Samlafo-Adams <101113451+samlafoedward1@users.noreply.github.com>
2023-10-27 23:38:16 +0200

    adding local react repo to reomte repo

---
+++ src/Greeting.jsx
@@ -0,0 +1,10 @@
+function Greeting() {
+  return (
+    <>
+      <h1>&quot; Hello, have a good day! &quot;</h1>
+      <p>I like boiled Yam</p>
+    </>
+  );
+}
+
+export default Greeting;
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Greeting from "./Greeting.jsx";
+import Express from "./Express.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+    <Greeting />
+    <Express />
+  </React.StrictMode>
+);
