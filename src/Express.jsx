9d438c425337698df04d598ff8b82cc8dfa3b267
Edward Samlafo-Adams <101113451+samlafoedward1@users.noreply.github.com>
2023-10-29 00:44:17 +0200

    rendering using props

--- src/Express.jsx
+++ src/Express.jsx
@@ -1,13 +1,24 @@
+function ListItem(props) {
+  return <li>{props.animal}</li>;
+}
+
+function List(props) {
+  return (
+    <ul>
+      {props.animals.map((animal) => {
+        return <ListItem key={animal} animal={animal} />;
+      })}
+    </ul>
+  );
+}
+
 function App() {
   const animals = ["Lion", "Cow", "Snake", "Lizard"];
+
   return (
     <div>
       <h1>Animals: </h1>
-      <ul>
-        {animals.map((animal) => {
-          return <li key={animal}>{animal}</li>;
-        })}
-      </ul>
+      <List animals={animals} />
     </div>
   );
 }
