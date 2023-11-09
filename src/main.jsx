cdfb69ed930cb3713057fe8f4de0ab064018b9ed
Edward Samlafo-Adams <101113451+samlafoedward1@users.noreply.github.com>
2023-11-10 00:28:58 +0100

    still practicing with reacts props

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import App from "./App.jsx";
 import "./index.css";
 import Greeting from "./Greeting.jsx";
 import Express from "./Express.jsx";
+import Motivation from "./Motivation.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <Greeting />
     <Express />
+    <Motivation />
   </React.StrictMode>
 );
